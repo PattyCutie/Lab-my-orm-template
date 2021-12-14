f2b46686e7ad6ece6db8f28aef67a9bc4b2e7b2a
PattyCutie <iamsureiamcute@gmail.com>
2021-12-14 14:04:28 +0000

    Lab:Created ORM template

---
+++ settings.py
@@ -0,0 +1,17 @@
+# PostgresSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'MTIxMzQtaWFtc3Vy',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'standalone',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
